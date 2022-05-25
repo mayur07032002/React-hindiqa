e714764df9771d443f4d1060f2ece353b8afb97e
Mayur Rawte <66206145+mayur07032002@users.noreply.github.com>
2022-05-25 12:18:47 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
-import './App.css'
-import React, {useEffect,useState} from 'react';
+import React from 'react';
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  
-  const [context,setpara] = useState(""); 
-  const [question,setques] = useState("");
-
-  function predict()
-  {
-    console.warn({context,question})
-    let data={context,question}
-    fetch("https://hindi-qa.herokuapp.com/",{
-      method:'POST',
-      mode: 'no-cors',
-      headers:{
-        'Accept':'application/json',
-        'Content-Type':'application/json'
-      },
-      body:JSON.stringify(data)
-      
-    }).then((result)=>{
-      console.warn("result",result)
+function App(){
+    
+    
+    const fetchdata = () => {
+        
+        axios.get("https://www.boredapi.com/api/activity")
+        .then((res)=>{
+            console.log(res);
+        })
+        
     }
+    const user = {
+        context:"my name is mayur",
+        question:"what is my name"
+    }
+
+    const createpost = ()=>{
+       
+        axios.post('https://hindi-qa.herokuapp.com/',user)
+            .then((res)=>{
+                console.log(res.data)
+            })
+    }
+    return(
+        <>
+        <div className='container my-3'>
+        <form>
+            <div className="mb-3">
+                <label className="form-label">context</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                <div id="emailHelp" className="form-text">.</div>
+            </div>
+            <div className="mb-2">
+                <label  className="form-label">question</label>
+                <input type="password" className="form-control" id="exampleInputPassword1"/>
+            </div>
+            <button type="submit" className="btn btn-primary" onClick={createpost}>Submit</button><br/><br/>
+            <button type="submit" className="btn btn-primary" onClick={fetchdata}>Submit</button>
+            </form>
+        </div>
+        </>
     )
-  }
-  
-  return (
-    <div className="App">
-      <h1>Question - Answering</h1>
-      <input type="text" name="para" value={context} onChange={(e)=>{setpara(e.target.value)}} placeholder='paragraph'></input> <br/> <br/>
-      <input type="text" name="ques" value={question} onChange={(e)=>{setques(e.target.value)}} placeholder='question'></input> <br/> <br/>
-      <button type="button" onClick={predict}>Predict</button>
-      <p>{context}</p>
-    </div>
-  )
 }
 
 export default App;
